d15bde5e37add803c6bc6b1c200ce508874e1717
dengdongming <2885804915@qq.com>
2020-05-18 15:59:29 +0800

    质检设置接口

--- src/views/setting/qualityTesting/index.vue
+++ src/views/setting/qualityTesting/index.vue
@@ -3,18 +3,18 @@
        <a-page-header title="质检设置" />
        <div>
           <a-tabs :activeKey="activeKey" @change="clickTabs">
-            <a-tab-pane key="1" tab="会话质检项"></a-tab-pane>
-            <a-tab-pane key="2" tab="电话质检项目"></a-tab-pane>
+            <a-tab-pane :key="0" tab="会话质检项"></a-tab-pane>
+            <a-tab-pane :key="1" tab="电话质检项目"></a-tab-pane>
           </a-tabs>
           <div class="flex btnArea" >
-            <p>当前评分项目总分范围：-5-15</p>
+            <p>当前评分项目总分范围：{{range}}</p>
             <a-button type="primary" @click="addScoreItem">添加评分项</a-button>
           </div>
-          <div>
-            <a-table :columns="columns"  :data-source="dataSource">
-                <div slot="action" slot-scope="row">
-                  <span class="blue" style="margin-right:10px;" @click="editItem">编辑</span>
-                  <span class="blue" @click="deleteItem">删除</span>
+          <div> 
+            <a-table :columns="columns"  :data-source="dataSource"  :pagination="false" :rowKey="record => record.id">
+                <div slot="action" slot-scope="text,record,index">
+                  <span class="blue" style="margin-right:10px;" @click="editItem(record)">编辑</span>
+                  <span class="blue" @click="deleteItem(record.id)">删除</span>
                 </div>
             </a-table>
           </div>
@@ -27,20 +27,20 @@
             :label-col="labelCol" 
             :wrapper-col="wrapperCol"
             >
-              <a-form-model-item label="评分项" prop="name">
-                <a-input v-model="addScoreFormData.name"/>
+              <a-form-model-item label="评分项" prop="gradeName">
+                <a-input v-model="addScoreFormData.gradeName"/>
               </a-form-model-item>
-              <a-form-model-item label="说明" prop="explain">
-                <a-input v-model="addScoreFormData.explain"/>
+              <a-form-model-item label="说明" prop="gradeExplain">
+                <a-input v-model="addScoreFormData.gradeExplain"/>
               </a-form-model-item>
-              <a-form-model-item label="评分类型" prop="type">
-                <a-radio-group v-model="addScoreFormData.type" :default-value="1">
-                  <a-radio :value="1"> 加分项</a-radio>
-                  <a-radio :value="2"> 扣分项 </a-radio>
+              <a-form-model-item label="评分类型" prop="gradeType">
+                <a-radio-group v-model="addScoreFormData.gradeType" :default-value="1">
+                  <a-radio :value="0"> 加分项</a-radio>
+                  <a-radio :value="1"> 扣分项 </a-radio>
                 </a-radio-group>
               </a-form-model-item>
-              <a-form-model-item label="得分分值" prop="score">
-                <a-input v-model="addScoreFormData.explain"/>
+              <a-form-model-item label="得分分值" prop="gradeLimit">
+                <a-input-number v-model="addScoreFormData.gradeLimit"/>
               </a-form-model-item>
             </a-form-model>
         </a-modal>
@@ -59,25 +59,26 @@ export default {
         return {
           labelCol: { span: 4 },
           wrapperCol: { span: 14 },
-          activeKey:'1',
+          activeKey:0,
           addScoreShow:false,
           addScoreFormData:{
-            name:'',
-            explain:'',
-            score:'',
-            type:''
+            gradeName:'',
+            gradeExplain:'',
+            gradeLimit:'',
+            gradeType:0
           },
+          range:'',
           rules:{
-            name: [
+            gradeName: [
               { required: true, message: '请输入  ', trigger: 'blur' },
             ],
-            explain: [
+            gradeExplain: [
               { required: true, message: '请输入  ', trigger: 'blur' },
             ],
-            score: [
-              { required: true, message: '请输入  ', trigger: 'blur' },
+            gradeType: [
+              { required: true, message: '请选择  ', trigger: 'blur' },
             ],
-            type: [
+            gradeLimit: [
               { required: true, message: '请输入  ', trigger: 'blur' },
             ],
           },
@@ -92,24 +93,30 @@ export default {
           columns:[
             {
               title: '评分项',
-              dataIndex: 'name',
+              dataIndex: 'gradeName',
               key: '1',
-              
             },
              {
               title: '说明',
-              dataIndex: 'explain',
+              dataIndex: 'gradeExplain',
               key: '2',
               width: '20%',
             },
              {
               title: '评分类型',
-              dataIndex: 'type',
+              dataIndex: 'gradeType',
               key: '3',
+              customRender:(value)=>{
+                let obj = {
+                  '0':'加分项',
+                  '1':'扣分项'
+                }
+                return obj[value]
+              }
             },
             {
               title: '得分范围',
-              dataIndex: 'scroe',
+              dataIndex: 'gradeLimit',
               key: '4',
             },
              {
@@ -122,9 +129,19 @@ export default {
           ]
         }
     },
-    created(){},
+    created(){
+      this.getList()
+    },
     mounted(){},
     methods: {
+      getList(){
+        this.Request.get('/config/qc/getListJson?type='+this.activeKey).then(res => {
+          console.log('质检设置list',res)
+          let list = res.data.list
+          this.dataSource = list
+          this.range = res.data.totalGradeMin + '~' + res.data.totalGradeMax
+        })
+      },
       clickTabs(key){
         this.activeKey = key
       },
@@ -135,6 +152,13 @@ export default {
         this.$refs.addScoreForm.validate(valid=>{
           if(valid){
             console.log(this.addScoreFormData)
+            let params = {...this.addScoreFormData,type:this.activeKey}
+            this.Request.post('/config/qc/saveGrade',params).then(res => {
+              console.log('质检设置保存',res)
+              this.$message.success('保存成功')
+              this.addScoreShow = false
+              this.getList()
+            })
           }
         })
       },
@@ -144,11 +168,34 @@ export default {
       addScoreItem(){
         this.addScoreShow = true
       },
-      editItem(){
-        this.addScoreShow = true
+      editItem(record){
+      this.addScoreShow = true
+      console.log('编辑',record)
+      let {gradeName,gradeExplain,gradeLimit,gradeType} = record
+      this.addScoreFormData = {
+          gradeName,
+          gradeExplain,
+          gradeLimit,
+          gradeType
+        }
+      // this.handleAddScoreOk()
       },
-      deleteItem(){
-          this.$message.success('删除成功')
+      deleteItem(id){
+        let that = this
+         this.$confirm({
+            title: '',
+            content: <div style="color:red;">删除评分项会改变现有评分内容，是否确认删除？</div>,
+            onOk() {
+              that.Request.post('/config/qc/deleteGrade',{id}).then(res => {
+                that.$message.success('删除成功')
+                that.getList()
+                console.log(res)
+              })
+            },
+            onCancel() {
+           
+            },
+          });
       } 
     }
 }
