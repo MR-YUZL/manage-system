973d7deea2635c02bf5207bfc8d0e6f5aadcafc7
xiaoxiannv <2292025262@qq.com>
2020-06-05 09:29:12 +0800

    数据分析

--- src/views/dataAnalysis/orderAnalysis/index.vue
+++ src/views/dataAnalysis/orderAnalysis/index.vue
@@ -10,22 +10,30 @@
       <span></span>
       <a-button type="primary">导出</a-button>
     </div>
-    <a-table :columns="columns" :dataSource="dataSource" :pagination="false"></a-table>
-    <a-table :columns="detailColumns" :dataSource="detailDataSource" :pagination="false"></a-table>
+    <a-table
+      :columns="columns"
+      :dataSource="dataSource"
+      :pagination="false"
+      :customRow="handleClickRow"
+    ></a-table>
+    <a-table v-if="detailShowTable" :columns="detailColumns" :dataSource="detailDataSource" :pagination="false"></a-table>
   </div>
 </template>
 
 <script>
-import api from "@/api/customerCenter";
+import api from "@/api/analysis";
 import FormModelSearchForm from "@/components/Search/FormModelSearchForm";
+import moment from 'moment';
 export default {
   data() {
     return {
+      detailShowTable:false,
+      searchParams: {},
       searchFormList: [
         {
           type: "rangepicker",
           name: "inputDateStart",
-          label: "查询时间"
+          label: "查询时间",
         },
         {
           type: "select",
@@ -37,18 +45,132 @@ export default {
           optionLabel: "id"
         }
       ],
-      defaultSearchFormValues: {},
-      columns: [],
+      defaultSearchFormValues: {
+        inputDateStart:[moment().format("YYYY-MM-DD"),moment().format("YYYY-MM-DD")]
+      },
+      columns: [
+        {
+          title: "客服姓名",
+          dataIndex: "inputAccName",
+          key: "inputAccName"
+        },
+        {
+          title: "创建工单量",
+          dataIndex: "createCount",
+          key: "createCount"
+        },
+        {
+          title: "受理工单量",
+          dataIndex: "receiverCount",
+          key: "receiverCount"
+        },
+        {
+          title: "完结工单量",
+          dataIndex: "endCount",
+          key: "endCount"
+        },
+        {
+          title: "转接工单量",
+          dataIndex: "transferCount",
+          key: "transferCount"
+        },
+        {
+          title: "跟进工单量",
+          dataIndex: "followCount",
+          key: "followCount"
+        },
+        {
+          title: "平均首次响应时间",
+          dataIndex: "firstResponseTime",
+          key: "firstResponseTime"
+        },
+        {
+          title: "平均处理时间",
+          dataIndex: "responseTime",
+          key: "responseTime"
+        }
+      ],
       dataSource: [],
-      detailColumns: [],
+      detailColumns: [
+        {
+          title: "日期",
+          dataIndex: "firstResponseTime",
+          key: "firstResponseTime"
+        },
+        {
+          title: "客服姓名",
+          dataIndex: "inputAccName",
+          key: "inputAccName"
+        },
+        {
+          title: "创建工单量",
+          dataIndex: "createCount",
+          key: "createCount"
+        },
+        {
+          title: "受理工单量",
+          dataIndex: "receiverCount",
+          key: "receiverCount"
+        },
+        {
+          title: "完结工单量",
+          dataIndex: "endCount",
+          key: "endCount"
+        },
+        {
+          title: "转接工单量",
+          dataIndex: "transferCount",
+          key: "transferCount"
+        },
+        {
+          title: "跟进工单量",
+          dataIndex: "followCount",
+          key: "followCount"
+        },
+      ],
       detailDataSource: []
     };
   },
   components: {
     FormModelSearchForm
   },
+  mounted() {
+    this.getOrderAccList();
+    console.log(this.defaultSearchFormValues.inputDateStart,'123******')
+  },
   methods: {
-    prevHandleSubmit() {}
+    moment,
+    handleClickRow(record,index){
+      return{
+        on:{
+          click:()=>{
+            this.detailShowTable = true;
+            let dateObj = Object.assign({}, this.defaultSearchFormValues,this.searchParams);
+            let dateArr = dateObj.inputDateStart
+            let params = {
+              startDate:dateArr[0],
+              endDate:dateArr[1],
+              inputAcc:record.inputAcc
+            }
+            api.orderDetailList(params).then(res=>{
+              console.log('工单工作分析明细',res)
+              this.detailDataSource = res.data.list;
+            })
+            console.log(record.inputAcc,index);
+          }
+        }
+      }
+    },
+    getOrderAccList() {
+      api.orderAccList(this.searchParams).then(res => {
+        console.log("工单工作分析", res);
+        this.dataSource = res.data.list;
+      });
+    },
+    prevHandleSubmit(val) {
+      this.searchParams = Object.assign({}, this.searchParams, val);
+      this.getOrderAccList();
+    }
   }
 };
 </script>
--- src/views/dataAnalysis/orderClassification/index.vue
+++ src/views/dataAnalysis/orderClassification/index.vue
@@ -1,20 +1,54 @@
 <template>
   <div>
-    <a-page-header title="工单统计" style="padding:16px 0;" />
+    <a-page-header title="工单分类统计" style="padding:16px 0;" />
     <FormModelSearchForm
       :defaultFormValues="defaultSearchFormValues"
       :formList="searchFormList"
       @prevHandleSubmit="prevHandleSubmit"
     />
+    <div class="circleFlex">
+      <div>
+        <CircleChart
+          id="circleChart1"
+          :echartObj="circleObj1"
+          styles="width:100%;height:300px;margin:auto"
+        />
+      </div>
+      <div class="orderType2 pr">
+        <a-select @change="firstTypeFn" v-model="orderSel2" style="width: 174px;">
+          <a-select-option v-for="(item,index) in orderTypeList1" :key="index" :value="item.id">{{item.name}}</a-select-option>
+        </a-select>
+        <CircleChart
+          id="circleChart2"
+          :echartObj="circleObj2"
+          styles="width:100%;height:300px;margin:auto"
+        />
+      </div>
+      <div class="orderType3 pr">
+        <a-select v-model="orderSel3" style="width: 174px;">
+          <a-select-option v-for="(item,index) in orderTypeList2" :key="index" :value="item.id">{{item.name}}</a-select-option>
+        </a-select>
+        <CircleChart
+          id="circleChart3"
+          :echartObj="circleObj3"
+          styles="width:100%;height:300px;margin:auto"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import api from "@/api/customerCenter";
+import api from "@/api/analysis";
 import FormModelSearchForm from "@/components/Search/FormModelSearchForm";
+import CircleChart from "@/views/dataAnalysis/circleChart";
 export default {
   data() {
     return {
+      orderTypeList1:[],
+      orderTypeList2:[],
+      orderSel2:'',
+      orderSel3:'',
       searchFormList: [
         {
           type: "rangepicker",
@@ -31,17 +65,149 @@ export default {
           optionLabel: "id"
         }
       ],
-      defaultSearchFormValues: {}
+      defaultSearchFormValues: {},
+      circleObj1: {
+        legend: [
+          "电话接待",
+          "网页接待",
+          "微信公众号",
+          "微信个人号",
+          "微信小程序",
+          "QQ接待"
+        ],
+        series: [
+          { value: 335, name: "电话接待" },
+          { value: 310, name: "网页接待" },
+          { value: 234, name: "微信公众号" },
+          { value: 135, name: "微信个人号" },
+          { value: 1548, name: "微信小程序" },
+          { value: 1548, name: "QQ接待" }
+        ],
+        total: [
+          {
+            value: 0,
+            name: "总接待量",
+            itemStyle: { normal: { color: "#ffffff" } }
+          }
+        ]
+      },
+      circleObj2: {
+        legend: [
+          "电话接待",
+          "网页接待",
+          "微信公众号",
+          "微信个人号",
+          "微信小程序",
+          "QQ接待"
+        ],
+        series: [
+          { value: 335, name: "电话接待" },
+          { value: 310, name: "网页接待" },
+          { value: 234, name: "微信公众号" },
+          { value: 135, name: "微信个人号" },
+          { value: 1548, name: "微信小程序" },
+          { value: 1548, name: "QQ接待" }
+        ],
+        total: [
+          {
+            value: 0,
+            name: "总接待量",
+            itemStyle: { normal: { color: "#ffffff" } }
+          }
+        ]
+      },
+      circleObj3: {
+        legend: [
+          "电话接待",
+          "网页接待",
+          "微信公众号",
+          "微信个人号",
+          "微信小程序",
+          "QQ接待"
+        ],
+        series: [
+          { value: 335, name: "电话接待" },
+          { value: 310, name: "网页接待" },
+          { value: 234, name: "微信公众号" },
+          { value: 135, name: "微信个人号" },
+          { value: 1548, name: "微信小程序" },
+          { value: 1548, name: "QQ接待" }
+        ],
+        total: [
+          {
+            value: 0,
+            name: "总接待量",
+            itemStyle: { normal: { color: "#ffffff" } }
+          }
+        ]
+      },
+      searchParams: {}
     };
   },
   components: {
-    FormModelSearchForm
+    FormModelSearchForm,
+    CircleChart
+  },
+  mounted() {
+    this.getOrderTypeChartJson();
+    this.getOrderTypeSelectJson();
   },
   methods: {
-    prevHandleSubmit() {}
+    firstTypeFn(val){
+      console.log(val)
+      this.orderSel2 = val;
+      this.fn()
+    },
+    getOrderTypeChartJson() {
+      api.orderTypeChartJson(this.searchParams).then(res => {
+        console.log(res, "工单分类统计图标数据");
+      });
+    },
+    getOrderTypeSelectJson() {
+      let promise = new Promise((resolve,reject)=>{
+        api.orderTypeSelectJson({ type: 1, pid: 0 }).then(res => {
+          resolve(res);
+          console.log(res, "工单分类右上角下拉框2");
+          this.orderTypeList1 = res.data.list;
+          this.orderSel2 = this.orderTypeList1[0].id;
+        });
+
+      });
+      promise.then(res=>{
+        this.fn()
+      })
+    },
+    fn(){
+      api.orderTypeSelectJson({ type: 1, pid: this.orderSel2 }).then(res => {
+          console.log(res, "工单分类右上角下拉框3");
+          if(res.data.list.length){
+            this.orderTypeList2 = res.data.list;
+            this.orderSel3 = this.orderTypeList2[0].id;
+          }
+        });
+    },
+    prevHandleSubmit(val) {
+      this.searchParams = Object.assign({}, this.searchParams, val);
+      this.getOrderTypeChartJson();
+    }
   }
 };
 </script>
 
 <style lang="less" scoped>
+.circleFlex {
+  display: flex;
+  div {
+    flex: 1;
+  }
+  .pr{
+    position: relative;
+    .ant-select{
+      position: absolute;
+      right: 0;
+      top: 0;
+      z-index: 10
+    }
+  }
+}
 </style>
