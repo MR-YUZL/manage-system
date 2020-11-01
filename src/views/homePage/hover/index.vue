cd2ebb7abe38b3915d0e048952c475cd61663a52
yuze <932906404@qq.com>
2020-11-01 23:04:43 +0800

    canvas

--- src/views/homePage/hover/index.vue
+++ src/views/homePage/hover/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="hover">
     <section class="lists">
       <a href="#" class="box">
         <img :src="require('@/assets/logo.png')" />
@@ -21,7 +21,7 @@
         <p>vue的名字</p>
       </div>
     </section> -->
-    <canvas id="myCanvas" :width="200 | numFormat" height="100">
+    <canvas id="myCanvas" :width="800 | numFormat" height="500" class="blur">
       您的浏览器不支持 HTML5 canvas 标签。
     </canvas>
     <a-button-group>
@@ -45,12 +45,12 @@ export default {
   },
   watch: {
     height(val) {
-      this.init();
+      this.drow();
     },
   },
   created() {},
   mounted() {
-    this.init();
+    this.drow();
   },
   methods: {
     increase() {
@@ -67,123 +67,158 @@ export default {
       }
       this.height = height;
     },
-    init() {
+    drow() {
       let c = document.getElementById("myCanvas");
-      let ctx = c.getContext("2d");
-      ctx.beginPath()
-      ctx.clearRect(0,0,200,100)
-      ctx.lineWidth = 2;
-      ctx.moveTo(0, 1);
-      ctx.lineTo(50, 1);
-      ctx.lineTo(50, this.height);
-      ctx.lineTo(100, this.height);
+      let ctx = c.getContext("2d"); //创建一个2d的画笔
+      // ctx.beginPath();     //清除路径
+      // ctx.clearRect(0, 0, 200, 100);  //清空指定矩形区域
+      // ctx.lineWidth = 2;  //线段宽度
+      // ctx.moveTo(0, 1);   //开始位置
+      // ctx.lineTo(50, 1);  //第二个点的位置
+      // ctx.lineTo(50, this.height);
+      // ctx.lineTo(100, this.height);
+      // ctx.stroke();  //画折线
+
+      // ctx.fillStyle = 'rgb(200,0,0)'  //颜色需要在绘制之前
+      // ctx.fillRect(10,10,350,300)  //画一个矩形（x，y，width，height）  填充矩形
+
+      // ctx.fillStyle = 'rgba(0,0,200,0.5)'
+      // ctx.fillRect(30,30,350,300)
+
+      // ctx.strokeStyle ='rgb(200,0,0)'
+      // ctx.strokeRect(10,10,350,300)
+
+      // ctx.strokeStyle ='rgba(0,0,200,0.5)'
+      // ctx.strokeRect(30,30,350,300)   //描边矩形
+
+      // ctx.fillRect(30,30,100,100)
+      // ctx.clearRect(50,50,60,60)
+      // ctx.strokeRect(60,60,40,40)
+      ctx.beginPath();
+      ctx.moveTo(75, 50);
+      ctx.lineTo(100, 75);
+      ctx.lineTo(100, 25);
+      // ctx.lineTo(75, 50);
+      // ctx.fill()
+        ctx.closePath()  //闭合要写在绘制前面
       ctx.stroke();
+    
     },
   },
 };
 </script>
 <style lang="less" scoped>
-.lists {
-  display: flex;
-  margin-top: 20px;
-  justify-content: center;
-  .box,
-  .box2,
-  .box3,
-  .box4 {
-    width: 100px;
-    height: 100px;
-    transition: all 0.3s;
-    background: #ffffff;
-    &:not(:last-child) {
-      margin-right: 15px;
-    }
-    img {
-      width: 100%;
-      height: 100%;
-    }
-  }
+.hover {
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  .lists {
+    display: flex;
+    margin-top: 20px;
+    justify-content: center;
 
-  .box {
-    &:hover {
-      transform: translateY(-6px);
+    .box,
+    .box2,
+    .box3,
+    .box4 {
+      width: 100px;
+      height: 100px;
+      transition: all 0.3s;
+      background: #ffffff;
+      &:not(:last-child) {
+        margin-right: 15px;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+      }
     }
-  }
-  .box2 {
-    &:hover {
-      transform: scale(1.03);
-    }
-  }
-  .box3 {
-    overflow: hidden;
-    img {
-      transition: all 0.5s;
+
+    .box {
       &:hover {
-        transform: scale(1.1);
+        transform: translateY(-6px);
+      }
+    }
+    .box2 {
+      &:hover {
+        transform: scale(1.03);
+      }
+    }
+    .box3 {
+      overflow: hidden;
+      img {
+        transition: all 0.5s;
+        &:hover {
+          transform: scale(1.1);
+        }
+      }
+    }
+    .box4 {
+      img {
+        transition: all 0.5s;
+        &:hover {
+          opacity: 0.8;
+        }
+      }
+    }
+    //   .box ~ .box {  //第二个box加外边距
+    //     margin-left: 15px;
+    //   }
+
+    .lists2 {
+      position: relative;
+      z-index: 1;
+      display: inline-block;
+      overflow: hidden;
+      background: #3085a3;
+      text-align: center;
+      cursor:pointer img {
+        width: 200px;
+        height: 200px;
+        opacity: 1;
+        transition: opacity 0.35s, transform 0.35s;
+        float: left;
+      }
+      &:hover img {
+        opacity: 0.5;
+      }
+      &:hover h3 {
+        transform: translate3d(0, 0, 0);
+        opacity: 1;
+      }
+      &:hover p {
+        opacity: 1;
+        transform: translate3d(0, 0, 0) scale(1);
+      }
+      h3 {
+        margin-top: 20%;
+        transition: transform 0.35s;
+        transform: translate3d(0, 20px, 0);
+        opacity: 0;
+        color: #fff;
+        font-size: 16px;
+      }
+      p {
+        margin: 10px 0 0;
+        padding: 15px;
+        border: 1px solid #fff;
+        opacity: 0;
+        transition: opacity 0.35s, transform 0.35s;
+        transform: translate3d(0, 20px, 0) scale(1.1);
+        color: #fff;
+      }
+      .box5 {
+        width: 200px;
+        height: 200px;
+        padding: 15px;
+        position: absolute;
+        box-sizing: border-box;
       }
     }
   }
-  .box4 {
-    img {
-      transition: all 0.5s;
-      &:hover {
-        opacity: 0.8;
-      }
-    }
-  }
-  //   .box ~ .box {  //第二个box加外边距
-  //     margin-left: 15px;
-  //   }
-
-  .lists2 {
-    position: relative;
-    z-index: 1;
-    display: inline-block;
-    overflow: hidden;
-    background: #3085a3;
-    text-align: center;
-    cursor:pointer img {
-      width: 200px;
-      height: 200px;
-      opacity: 1;
-      transition: opacity 0.35s, transform 0.35s;
-      float: left;
-    }
-    &:hover img {
-      opacity: 0.5;
-    }
-    &:hover h3 {
-      transform: translate3d(0, 0, 0);
-      opacity: 1;
-    }
-    &:hover p {
-      opacity: 1;
-      transform: translate3d(0, 0, 0) scale(1);
-    }
-    h3 {
-      margin-top: 20%;
-      transition: transform 0.35s;
-      transform: translate3d(0, 20px, 0);
-      opacity: 0;
-      color: #fff;
-      font-size: 16px;
-    }
-    p {
-      margin: 10px 0 0;
-      padding: 15px;
-      border: 1px solid #fff;
-      opacity: 0;
-      transition: opacity 0.35s, transform 0.35s;
-      transform: translate3d(0, 20px, 0) scale(1.1);
-      color: #fff;
-    }
-    .box5 {
-      width: 200px;
-      height: 200px;
-      padding: 15px;
-      position: absolute;
-      box-sizing: border-box;
-    }
+  .blur {
+    box-shadow: 0 0 10px #3085a3;
+    margin: 0 auto;
   }
 }
 </style>
